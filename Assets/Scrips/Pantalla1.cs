c4e2f72dc9e9683da6a3102f23b20cf9044e7d9e
Facujuli6 <jfacundo.ortiz@gmail.com>
2025-10-16 18:39:25 -0300

    Menu
    Avance

--- Assets/Scrips/Pantalla1.cs
+++ Assets/Scrips/Pantalla1.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Pantalla1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Button botonInicio;
+    [SerializeField] private string nombreEscenaMenu = "Menu";
+
+    private void Awake()
     {
-        
+        if (botonInicio != null)
+        {
+            botonInicio.onClick.AddListener(IrAlMenu);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (botonInicio != null)
+        {
+            botonInicio.onClick.RemoveListener(IrAlMenu);
+        }
+    }
+
+    public void IrAlMenu()
+    {
+        if (!string.IsNullOrWhiteSpace(nombreEscenaMenu))
+        {
+            SceneManager.LoadScene(nombreEscenaMenu);
+        }
+        else
+        {
+            Debug.LogWarning("Pantalla1: nombreEscenaMenu está vacío, asigna el nombre de la escena del menú.");
+        }
     }
 }
